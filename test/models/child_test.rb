ecaa6054e028b71f7c7bb7156a0a1ef16a4a95b5
yaoleo34 <yaoleo34@gmail.com>
2021-02-05 15:25:26 -0800

    added relationship macros to child

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ChildTest < ActiveSupport::TestCase
   # TODO: Relationship macros
-  
+  should have_many(:chores)
+  should have_many(:tasks).through(:chores)
   # TODO: Validation macros
 
   # TODO: Context testing
