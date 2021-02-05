9b3cb77e66fa74e91b4262dda0332dced62fcd4d
yaoleo34 <yaoleo34@gmail.com>
2021-02-05 15:22:19 -0800

    test now uses simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'contexts'
+require 'simplecov'
+SimpleCov.start 'rails'
 
 
 class ActiveSupport::TestCase
@@ -23,7 +25,7 @@ class ActiveSupport::TestCase
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
